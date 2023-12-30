a117d8939911b7d4e0e4e1afd1e329db478b8b45
= <108344292+NerdyXG@users.noreply.github.com>
2023-12-30 13:45:11 +0100

    Connect database with application to fetch data from the database dynamically in real time

--- places/models.py
+++ places/models.py
@@ -2,16 +2,9 @@ from django.db import models
 
 # Create your models here.
 
-class Destination:
-    def __init__(self, id, image="Image N/A", name="Name N/A", description="Description N/A", price="Price N/A", special_offer = False):
-        self.id = id
-        self.image = image
-        self.name = name
-        self.description = description
-        self.price = price
-        self.special_offer = special_offer
-
-    # id: int
-    # name: str
-    # description: str
-    # price: int
+class Destination(models.Model):
+    image = models.ImageField(upload_to="destination_imgs")
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.IntegerField()
+    special_offer = models.BooleanField(default=False)
--- travello/settings.py
+++ travello/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "places.apps.PlacesConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'travello.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "travello",
+        "USER": "postgres",
+        "PASSWORD": "myadmin951478",
+        "HOST": "localhost"
     }
 }
 
@@ -126,3 +130,6 @@ STATIC_ROOT = BASE_DIR / "assets"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
