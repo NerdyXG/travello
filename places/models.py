6fc164aca02b38e8ace02c804586f6f97bcd684e
= <108344292+NerdyXG@users.noreply.github.com>
2023-12-30 11:33:52 +0100

    Make destination contents dynamic

--- places/models.py
+++ places/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Destination:
+    def __init__(self, id, image="Image N/A", name="Name N/A", description="Description N/A", price="Price N/A", special_offer = False):
+        self.id = id
+        self.image = image
+        self.name = name
+        self.description = description
+        self.price = price
+        self.special_offer = special_offer
+
+    # id: int
+    # name: str
+    # description: str
+    # price: int
